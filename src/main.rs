8ca29ae1aa4744c035c10762d40a2738887dc622
kayaba <kayaba.fluctlight@gmail.com>
2022-11-10 17:34:49 +0700

    customized build pipeline for Web target

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use wasm_bindgen::prelude::*;
 
 #[cfg(not(feature = "reload"))]
 use systems::*;
@@ -44,3 +45,26 @@ fn main() {
 }
 
 fn network_system() {}
+
+#[wasm_bindgen]
+extern "C" {
+	// Use `js_namespace` here to bind `console.log(..)` instead of just
+	// `log(..)`
+	#[wasm_bindgen(js_namespace = console)]
+	fn log(s: &str);
+
+	// The `console.log` is quite polymorphic, so we can bind it with multiple
+	// signatures. Note that we need to use `js_name` to ensure we always call
+	// `log` in JS.
+	#[wasm_bindgen(js_namespace = console, js_name = log)]
+	fn log_u32(a: u32);
+
+	// Multiple arguments too!
+	#[wasm_bindgen(js_namespace = console, js_name = log)]
+	fn log_many(a: &str, b: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+	log(&format!("Hello {}, from inside Rust!!!", name));
+}
